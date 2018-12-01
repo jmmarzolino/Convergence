a720ed73be95c2b15836f136d6cf00dd8bcc07e7
Jill Marzolino <jmarz001@ucr.edu>
2018-11-30 16:22:18 -0800

    fix header and file feed
    make file feed work as array job without naming everything 1 file...

--- fq.seqs.sh
+++ fq.seqs.sh
@@ -2,19 +2,19 @@
 
 #SBATCH --output=/rhome/jmarz001/bigdata/convergent_evolution/scripts/fq_seqs.stdout
 #SBATCH --job-name='seqs'
-#SBATCH --ntasks=1
+#SBATCH --ntasks=6
 #SBATCH --mem=32G
 #SBATCH --time=1:00:00
 #SBATCH -p batch
+#SBATCH --array=1-36
 
 DIR=/rhome/jmarz001/bigdata/convergent_evolution/data
 RES=/rhome/jmarz001/bigdata/convergent_evolution/args
 cd $DIR
-printf "RAW FASTQ's \n" >> $RES/file_reads
+SEQLIST=/rhome/jmarz001/bigdata/convergent_evolution/args/fastq_trim_seqs
 
+# get filenames from list
+FILE=$(head -n $SLURM_ARRAY_TASK_ID $SEQLIST | tail -n 1)
 
-for file in $DIR/*.fastq
-do
-  count=$(grep -c "^@" $file)
-  printf "$file \t $count \n" >> $RES/file_reads
-done
+COUNT=$(grep -c "^@" $FILE)
+printf "$FILE \t $COUNT \n" >> $RES/file_reads
