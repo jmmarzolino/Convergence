88d92efba7d9f2e15a16c98d09411a28c7c30727
Jill Marzolino <jmarz001@ucr.edu>
2019-04-02 13:37:56 -0700

    add the last 2 populations by chr

--- pipeline/con005_gvcf_tail.sh
+++ pipeline/con005_gvcf_tail.sh
@@ -230,3 +230,55 @@ cd $BAM
 region=$(head -n $SLURM_ARRAY_TASK_ID $CHR | tail -n 1)
 FILE=262_pool_merge_dup_marked.bam
 gatk HaplotypeCaller -R $REF -L ${region} -I $BAM/$FILE -O $SNP/262_${region}.g.vcf -ERC GVCF
+
+#########################################################################################################
+
+#!/bin/bash -l
+
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=60G
+#SBATCH --time=7-00:00:00
+#SBATCH --output=gatk_split_267.stdout
+#SBATCH --job-name="gatk_snp"
+#SBATCH --partition=intel
+#SBATCH --array=1-16
+
+module load java gatk/4.0.12.0 samtools picard
+PICARD=/opt/linux/centos/7.x/x86_64/pkgs/picard/2.18.3/lib/picard.jar
+REF=/rhome/jmarz001/shared/GENOMES/NEW_BARLEY/GENOME_SPLIT/barley_split_reference.fa
+CHR=/rhome/jmarz001/bigdata/convergent_evolution/small_sample/args/chr_intervals.list
+#
+SEQLIST=/rhome/jmarz001/bigdata/convergent_evolution/args/bams
+BAM=/rhome/jmarz001/bigdata/convergent_evolution/data/bam
+SNP=/rhome/jmarz001/bigdata/convergent_evolution/data/calls
+#
+cd $BAM
+region=$(head -n $SLURM_ARRAY_TASK_ID $CHR | tail -n 1)
+FILE=267_pool_merge_dup_marked.bam
+gatk HaplotypeCaller -R $REF -L ${region} -I $BAM/$FILE -O $SNP/267_${region}.g.vcf -ERC GVCF
+
+#########################################################################################################
+
+#!/bin/bash -l
+
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=60G
+#SBATCH --time=7-00:00:00
+#SBATCH --output=gatk_split_7L.stdout
+#SBATCH --job-name="gatk_snp"
+#SBATCH --partition=intel
+#SBATCH --array=1-16
+
+module load java gatk/4.0.12.0 samtools picard
+PICARD=/opt/linux/centos/7.x/x86_64/pkgs/picard/2.18.3/lib/picard.jar
+REF=/rhome/jmarz001/shared/GENOMES/NEW_BARLEY/GENOME_SPLIT/barley_split_reference.fa
+CHR=/rhome/jmarz001/bigdata/convergent_evolution/small_sample/args/chr_intervals.list
+#
+SEQLIST=/rhome/jmarz001/bigdata/convergent_evolution/args/bams
+BAM=/rhome/jmarz001/bigdata/convergent_evolution/data/bam
+SNP=/rhome/jmarz001/bigdata/convergent_evolution/data/calls
+#
+cd $BAM
+region=$(head -n $SLURM_ARRAY_TASK_ID $CHR | tail -n 1)
+FILE=7L_pool_merge_dup_marked.bam
+gatk HaplotypeCaller -R $REF -L ${region} -I $BAM/$FILE -O $SNP/7L_${region}.g.vcf -ERC GVCF
