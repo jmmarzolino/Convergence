0826be65c268d75e7bd75408936d76d21c7b9a1a
Jill Marzolino <jmarz001@ucr.edu>
2019-04-19 14:16:05 -0700

    still trying to get frequencies!

--- statistics/analysis/res001A_MAFs.sh
+++ statistics/analysis/res001A_MAFs.sh
@@ -5,13 +5,22 @@
 #SBATCH --time=2:00:00
 #SBATCH --output=res001_MAF.stdout
 #SBATCH --job-name='res001'
+#SBATCH --array=1-16
+
+POPS=/rhome/jmarz001/bigdata/convergent_evolution/args/populations
+NAME=$(head -n $SLURM_ARRAY_TASK_ID $POPS | tail -n 1)
+SHORT=$(basename $NAME | cut -d_ -f1)
 
 module load bcftools/1.8
 FILT=/rhome/jmarz001/bigdata/convergent_evolution/data/vcf/filter
 RESULT=/rhome/jmarz001/bigdata/convergent_evolution/results
-bcftools query -f '%CHROM %POS[\t%AD\t%DP]\n' $FILT/conevo.strand_bias_filt.vcf -o $RESULT/conevo_AF
 
+for file in $RESULT/
+do
+  bcftools query --regions-file $REGION -f '%CHROM %POS[\t%AD\t%DP]\n' $FILT/conevo.strand_bias_filt.vcf -o $RESULT/conevo_AF
+done
 
+############################################################
 cut -f1-3 conevo_AF > 10_AF
 cut -f1,4-5 conevo_AF > 16_AF
 cut -f1,6-7 conevo_AF > 1L_AF
@@ -24,25 +33,22 @@ cut -f1,18-19 conevo_AF > 262_AF
 cut -f1,20-21 conevo_AF > 267_AF
 cut -f1,22-23 conevo_AF > 2L_AF
 cut -f1,24-25 conevo_AF > 7L_AF
+############################################################
 
 #!/usr/bin/bash -l
 #SBATCH -p batch
-#SBATCH --ntasks=1
-#SBATCH --mem=30G
-#SBATCH --time=2-00:00:00
+#SBATCH --ntasks=5
+#SBATCH --mem-per-cpu=30G
+#SBATCH --time=02:00:00
 #SBATCH --output=res001A_MAF.stdout
 #SBATCH --job-name='res001'
 #SBATCH --array=1-12
 
 RESULT=/rhome/jmarz001/bigdata/convergent_evolution/results
-POPS=/rhome/jmarz001/bigdata/convergent_evolution/args/populations
-NAME=$(head -n $SLURM_ARRAY_TASK_ID $POPS | tail -n 1)
-SHORT=$(basename $NAME | cut -d_ -f1)
+cd $RESULT
+FILE=$(head -n $SLURM_ARRAY_TASK_ID files | tail -n 1)
+NAME=$(basename $FILE | cut -d_ -f1)
 
-for line in $RESULT/${SHORT}_AF
-do
-   A=$(cut $line -f1)
-   B=$(cut $line -f2 | cut -d, -f1)
-   C=$(cut $line -f3)
-   paste $A $B $C > $RESULT/${SHORT}_AF2B
-done
+cut $FILE -d, -f1 > ${NAME}_test2
+cut $FILE -f3 > ${NAME}_test3
+paste ${NAME}_test2 ${NAME}_test3 > ${NAME}_AF2
--- statistics/analysis/res001B_MAF.py
+++ statistics/analysis/res001B_MAF.py
@@ -2,64 +2,36 @@
 #SBATCH -p short
 #SBATCH --ntasks=1
 #SBATCH --mem=30G
-#SBATCH --time=00:20:00
-#SBATCH --output=res001B_MAF.stdout
+#SBATCH --time=02:00:00
 #SBATCH --job-name='res001B'
-
-ABANDONED
+#SBATCH --output=/rhome/jmarz001/bigdata/convergent_evolution/results/10_AF.stdout
 
 #set input file name
-InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/conevo_AF'
+InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/10_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/16_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/1L_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/24_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/250_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/255_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/257_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/258_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/262_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/267_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/2L_AF2'
+#InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/7L_AF2'
+
 #open input file
 InFile = open(InFileName, 'r')
 #Loop through each line in file
 for Line in InFile:
     Line = Line.strip('\n') # Split the file into lines
     ElementList = Line.split('\t') # Split the lines into elements
-    AlleleList = Element.split(',') # Split the elements by ',' character to split AD entries
-    print "ElementList:", ElementList #uncomment for debugging
-    #CHROM = ElementList[0]
-    #POS = ElementList[1]
-
-
-    #Retained = float(Altref[0])/float(ElementList[3])
-
-    #Retained = int(Retained * 100)
-
-    #Retained = str(Retained)
-
-    #OutputString = "%s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s" % \
-    #(CHROM, POS, AF1, AF2, AF3, AF4, AF5, AF6, AF7, AF8, AF9, AF10, AF11, AF12) # CHROM and POS first two col
-    #print OutputString+"\n"
+    #print "ElementList:", ElementList #uncomment for debugging
+    # ElementList: ['chr1H_1_279267716 799', '13', '13']
+    CHROM = ElementList[0]
+    FREQ = float(ElementList[1])/float(ElementList[2])
+    FREQ = str(FREQ)
+    OutputString = "%s\t %s" % \
+(CHROM, FREQ)
+    print OutputString+"\n"
 InFile.close()
-
-chr1H_1_279267716       [0]
- 799
- 13,0
- 13
- 13,2
- 15
- 0,0
- 0
- 6,0
- 6
- 6,2
- 8
- 8,0
- 8
-3,0
-3
-7,0
-7
-22,0
-22
-6,0
-6
-0,0
-0
-0,0
-0
-
-take pairs of columns, calculate AF for the population, print it out as a column and move onto the next
-printing a header column to make all this clear is desirable
-then loop this over all lines and write out
