5375d5c4270965aed7ccd03d87a42d84a585b18a
Jill Marzolino <jmarz001@ucr.edu>
2019-05-05 18:54:24 -0700

    fix type errors

--- statistics/analysis/site_freq_spectrum.py
+++ statistics/analysis/site_freq_spectrum.py
@@ -11,7 +11,7 @@ PopList = ['10', '16', '1L', '24', '250', '255', '257', '258', '262', '267', '2L
 bins = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.1]
 lines = 65381964
 for Pop in PopList:
-    InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/'+ 'site_frq_' + Pop'
+    InFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/'+ 'site_frq_' + Pop
     InFile = open(InFileName, 'r')
     OutFileName = '/rhome/jmarz001/bigdata/convergent_evolution/results/' + Pop + '_bins'
     OutFile = open(OutFileName, 'w+')
@@ -36,57 +36,60 @@ for Pop in PopList:
     s = 0
     t = 0
     u = 0
+    zero = 0
     unknown = 0
     for Line in InFile:
         # take in the frequency, put it in a bin
         LineList = Line.strip('\n')
         #print "LineList:", LineList #uncomment for debugging
-        if LineList == "na":
+        if Line == "na":
             u = u + 1
-        elif LineList > bins[0] & LineList < bins[1]:
+        elif Line == bins[0]:
+            zero = zero + 1
+        elif Line > bins[0] and Line < bins[1]:
             a = a + 1
-        elif LineList > bins[1] & LineList < bins[2]:
+        elif Line > bins[1] and Line < bins[2]:
             b = b + 1
-        elif LineList > bins[2] & LineList < bins[3]:
+        elif Line > bins[2] and Line < bins[3]:
             c = c + 1
-        elif LineList > bins[3] & LineList < bins[4]:
+        elif Line > bins[3] and Line < bins[4]:
             d = d + 1
-        elif LineList > bins[4] & LineList < bins[5]:
+        elif Line > bins[4] and Line < bins[5]:
             e = e + 1
-        elif LineList > bins[5] & LineList < bins[6]:
+        elif Line > bins[5] and Line < bins[6]:
             f = f + 1
-        elif LineList > bins[6] & LineList < bins[7]:
+        elif Line > bins[6] and Line < bins[7]:
             g = g + 1
-        elif LineList > bins[7] & LineList < bins[8]:
+        elif Line > bins[7] and Line < bins[8]:
             h = h + 1
-        elif LineList > bins[8] & LineList < bins[9]:
+        elif Line > bins[8] and Line < bins[9]:
             i = i + 1
-        elif LineList > bins[9] & LineList < bins[10]:
+        elif Line > bins[9] and Line < bins[10]:
             j = j + 1
-        elif LineList > bins[10] & LineList < bins[11]:
+        elif Line > bins[10] and Line < bins[11]:
             k = k + 1
-        elif LineList > bins[11] & LineList < bins[12]:
+        elif Line > bins[11] and Line < bins[12]:
             l = l + 1
-        elif LineList > bins[12] & LineList < bins[13]:
+        elif Line > bins[12] and Line < bins[13]:
             m = m + 1
-        elif LineList > bins[13] & LineList < bins[14]:
+        elif Line > bins[13] and Line < bins[14]:
             n = n + 1
-        elif LineList > bins[14] & LineList < bins[15]:
+        elif Line > bins[14] and Line < bins[15]:
             o = o + 1
-        elif LineList > bins[15] & LineList < bins[16]:
+        elif Line > bins[15] and Line < bins[16]:
             p = p + 1
-        elif LineList > bins[16] & LineList < bins[17]:
+        elif Line > bins[16] and Line < bins[17]:
             q = q + 1
-        elif LineList > bins[17] & LineList < bins[18]:
+        elif Line > bins[17] and Line < bins[18]:
             r = r + 1
-        elif LineList > bins[18] & LineList < bins[19]:
+        elif Line > bins[18] and Line < bins[19]:
             s = s + 1
-        elif LineList > bins[19] & LineList < bins[20]:
+        elif Line > bins[19] and Line < bins[20]:
             t = t + 1
         else:
             unknown = unknown + 1
     # when the bins are done, divide by lines
-    hist = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u]
+    hist = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, zero]
     proportional = [unknown]
     for i in hist:
         i = float(i/lines)
