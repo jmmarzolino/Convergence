6a4a12b0e7ec768913bd409c72bb044b7c6a7455
Jill Marzolino <jmarz001@ucr.edu>
2019-05-21 08:58:42 -0700

    update to remove sites with only 0 or 1 as all pop allele frequency

--- pipeline/filt001_AF_GT.sh
+++ pipeline/filt001_AF_GT.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks=2
 #SBATCH --mem=100G
 #SBATCH --time=2:00:00
-#SBATCH --output=con008_filtA.stdout
-#SBATCH --job-name='con008'
+#SBATCH --output=filt001_AF_GT.stdout
+#SBATCH --job-name='filt001'
 #SBATCH --array=1-16
 
 module load bcftools/1.8
@@ -16,12 +16,12 @@ NAME=$(basename "$FILE" | cut -d_ -f1-3)
 
 # get the filter metrics
 # bcftools stats [options] A.vcf.gz [B.vcf.gz]
-bgzip $SNPS/$FILE
-bcftools stats $SNPS/$FILE.gz > $FILTER/$NAME.stats
+#bgzip $SNPS/$FILE
+#bcftools stats $SNPS/$FILE.gz > $FILTER/$NAME.stats
 
 # extract allele frequency at each position
 #bcftools query -f '%CHROM %POS %AF\n' $FILE -o "$NAME"_AF.vcf
 #grep "^AF" $FILTER/"$NAME"_AF.vcf >> $FILTER/all_AFs
 
 # filter by site based on stats
-bcftools view -i 'F_MISSING<0.5 & DP>0 & DP<445 & QUAL>30 & N_ALT=1' $SNPS/$FILE -o $FILTER/$NAME.minalt.vcf
+bcftools view -i 'F_MISSING<0.5 & DP>0 & DP<445 & QUAL>30 & N_ALT=1 & AF < 1 & AF > 0' $SNPS/$FILE -o $FILTER/$NAME.minalt.vcf
